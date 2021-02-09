c89bd83a97d19fb1aae3f9099d69905031fa108e
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-02-09 02:04:17 -0500

    Started the contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import './../assets/styles/contact.css';
+
+function Contact() {
+  return (
+    <div>
+     <div className="container">
+       <h1>Contact Me</h1>
+       <p>
+         <a href="mailto:tamira.bell@gmail.com">
+           <i className="fas fa-envelope icon"> tamira.bell@gmail.com</i>
+          </a>
+       </p>
+
+       <p>
+         <a href="https://www.linkedin.com/in/tamira-bell" target="_blank" rel="noopener noreferrer">
+           <i className="fab fa-linkedin icon"> LinkedIn</i>
+         </a>
+       </p>
+
+       <p>
+         <a href="https://github.com/tbell-woG" target="_blank" rel="noopener noreferrer">
+           <i className="fab fa-github icon"> GitHub</i>
+         </a>
+       </p>
+     </div>
+    </div>
+    
+  );  
+}
+
+export default Contact;
