fac99fd40336fd42de7daedcc6b7972802163d71
Tamira Bell <74482916+tbell-woG@users.noreply.github.com>
2021-02-09 02:13:57 -0500

    Started the portfolio page

---
+++ src/pages/Portfolio.js
@@ -0,0 +1,49 @@
+import React, { Component } from 'react';
+import Card from './../components/Card';
+import projects from './../projects.json';
+import './../assets/styles/portfolio.css';
+
+class Portfolio extends Component {
+  // This keeps track of the project data
+  state = {
+    projects: projects,
+  }
+
+  render() {
+    // This maps through the project data and creates a card for each project
+    const projectsArr = this.state.projects;
+    const newProjectsArr = projectsArr.map((project) => 
+      <Card 
+        id={project.id}
+        name={project.name}
+        image={project.image}
+        description={project.description}
+        tryLink={project.tryLink}
+        repository={project.repository}
+        note={project.note}
+        technologies={project.technologies}
+      />
+    );
+
+    return(
+      <div>
+
+        <div className="container mb-5">
+          <h1>Portfolio</h1>
+          <h3>Here are some projects I completed.</h3>
+        </div>
+          
+        
+        <div className="flex-container">
+          
+          {/* This renders the newProjects array, which is an array of the cards */}
+          {newProjectsArr}
+
+        </div>
+      </div>
+        
+    );
+  }
+}
+
+export default Portfolio;
